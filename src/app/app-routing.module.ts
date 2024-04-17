e30eeb0bedb3cf9c4af3179282f807a800ff9743
NithyaScrumMaster <scrummaster@xyug.in>
2024-04-17 18:11:49 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './common/footer/footer.component';
 import { HeaderComponent } from './common/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
 
   { path: '', component: HomeComponent },
   { path: 'header', component: HeaderComponent },
   { path: 'footer', component: FooterComponent },
+  {path:'**',component:PagenotfoundComponent},
 ];
 
 @NgModule({
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const Environment = {
+    production: false,
+
+    // Live Keys
+    apiUrl: 'https://api.goldbox.gold/api/',
+
+    // apiUrl: 'http://stg-goldbox-api.goldsikka.com:8080/api/',
+
+}
