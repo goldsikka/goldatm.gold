8deedeb0480400a4ddac986ca47f2f24a1544e00
NithyaScrumMaster <148335013+NithyaScrumMaster@users.noreply.github.com>
2024-04-22 14:58:53 +0530

    Delete src directory

--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const Environment = {
-    production: false,
-
-    // Live Keys
-    apiUrl: 'https://api.goldbox.gold/api/',
-
-    // apiUrl: 'http://stg-goldbox-api.goldsikka.com:8080/api/',
-
-}
